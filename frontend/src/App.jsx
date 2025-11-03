38fd11e484a57c6dba7976e907d6a6a0a58fb4c5
povvithya viseth <povvithyaviseth@csus.edu>
2025-11-02 20:35:42 -0800

    Frontend and Backend connection part 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+
+function App() {
+  console.log("Chakra button rendered")
+  return (
+    <>
+      <Button>Hello!</Button>
+    </>
+  )
+}
+
+export default App
