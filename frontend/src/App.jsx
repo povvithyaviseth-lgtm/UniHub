362a1f58520b368d75c5bc8a3dc6edb43de4ef53
yenuliii <yenulidesilva@csus.edu>
2025-11-04 02:03:53 -0800

    before i delete the app.css n index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
-import Signup from './Signup.jsx'
-import HomePage from './HomePage.jsx'
-import StudentLogin from './StudentLogin.jsx'
-import AdminLogin from './AdminLogin.jsx'
-import AdminConsole from './AdminConsole.jsx'
-import ClubManagement from './ClubManagement.jsx'
-
+import { Box } from '@chakra-ui/react'
+import Signup from './pages/Signup.jsx'
+import HomePage from './pages/HomePage.jsx'
+import StudentLogin from './pages/StudentLogin.jsx'
+import AdminLogin from './pages/AdminLogin.jsx'
+import AdminConsole from './pages/AdminConsole.jsx'
+import ClubManagement from './pages/ClubManagement.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Signup />} />              {/* Default landing page */}
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/student-login" element={<StudentLogin />} />
-      <Route path="/admin-login" element={<AdminLogin />} />
-      <Route path="/admin-console" element={<AdminConsole />} />
-      <Route path="/club-management" element={<ClubManagement />} />
-    </Routes>
+    <Box p={4}>
+      <Routes>
+        <Route path="/" element={<Signup />} />              
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin-console" element={<AdminConsole />} />
+        <Route path="/club-management" element={<ClubManagement />} />
+      </Routes>
+    </Box>
   )
 }
 
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+// Signup.jsx (example)
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      <p>Work in progress...</p>
+    </div>
+  )
+}
