3154e4965b4e22bda306f599dedf9e0b47a78ef0
yenuliii <yenulidesilva@csus.edu>
2025-11-04 05:33:30 -0800

    signup page displays

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+  return (
+    <div>
+      <h1>Hello World — Signup Page</h1>
+      <p>If you can see this, everything is connected properly 🎉</p>
+    </div>
+  )
+}
+
+export default Signup
