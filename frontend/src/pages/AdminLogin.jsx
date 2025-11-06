75e0cd4be8227a2ff6c32313f51a3af12d33c645
povvithya viseth <povvithyaviseth@csus.edu>
2025-11-05 20:03:51 -0800

    Admin Sign Up page part 2

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -1,39 +1,62 @@
 const AdminLogin = () => {
   return (
-    <body style={{width: "100vw", height: "100vh", margin: 0, padding: 0, display: "flex", justifyContent: "center", alignItems: "center", background: "#F5F5F5"}}>
-        <div style={{alignSelf: 'stretch', alignSelf: 'stretch', paddingLeft: 56, paddingRight: 56, paddingTop: 125, paddingBottom: 125, background: '#F3F3F3', overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-        <div style={{width: 779, height: 692, position: 'relative', background: 'white', boxShadow: '0px 15px 30px rgba(0, 0, 0, 0.25)', overflow: 'hidden', borderRadius: 20}}>
-        <div style={{width: 724, height: 643, left: 27, top: 31, position: 'absolute', overflow: 'hidden'}}>
-        <div style={{width: 724, left: 0, top: 0, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'black', fontSize: 64, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Admin Portal</div>
-        <div style={{width: 353, left: 185, top: 92, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#5B5A5A', fontSize: 20.03, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Sign in to manage and moderate clubs.</div>
-        <div style={{width: 674, height: 59, left: 25, top: 200, position: 'absolute'}}>
-        <div style={{width: 674, height: 50, left: 0, top: 5, position: 'absolute', background: 'rgba(30, 64, 175, 0)', borderRadius: 10.02, border: '1.50px #EEEEEE solid'}} />
-        <div style={{width: 645, height: 50, left: 12, top: 5, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#ACACAC', fontSize: 17, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Enter your Email</div>
-        </div>
-        <div style={{width: 674, height: 59, left: 25, top: 298, position: 'absolute'}}>
-        <div style={{width: 674, height: 50, left: 0, top: 5, position: 'absolute', background: 'rgba(30, 64, 175, 0)', borderRadius: 10.02, border: '1.50px #EEEEEE solid'}} />
-        <div style={{width: 648, height: 50, left: 13, top: 9, position: 'absolute', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#ACACAC', fontSize: 17, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>**************************</div>
-        </div>
-        <div style={{width: 674, left: 25, top: 176, position: 'absolute', color: '#404040', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
-        <div style={{width: 674, left: 25, top: 274, position: 'absolute', color: '#404040', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
-        <div style={{width: 175, left: 25, top: 461, position: 'absolute', color: '#007D99', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Forgot Password?</div>
-        <div style={{width: 674, height: 59, left: 25, top: 372, position: 'absolute'}}>
-        <div style={{width: 674, height: 50, left: 0, top: 5, position: 'absolute', background: '#00550A', borderRadius: 10.02}} />
-        <div style={{width: 674, height: 50, left: 0, top: 5, position: 'absolute', textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: 'white', fontSize: 25.04, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Sign In As Admin</div>
-        </div>
-        <div style={{width: 674, height: 2, left: 25, top: 446, position: 'absolute', background: '#E6E6E6'}} />
-        <div style={{width: 674, height: 108, left: 25, top: 515, position: 'absolute', background: '#83A0FF', borderRadius: 15}} />
-        <div style={{width: 674, height: 108, left: 38, top: 515, position: 'absolute', background: '#E9F0FF', overflow: 'hidden', borderRadius: 15}}>
-        <div style={{width: 258, height: 72, left: 208, top: 18, position: 'absolute'}}>
-          <div style={{width: 240, left: 10, top: 0, position: 'absolute', color: '#404040', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Are you a Student?</div>
-          <div style={{width: 276, left: -23, top: 48, position: 'absolute', color: '#007D99', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Click to go to Student Login</div>
+    <body style="width: 100vw; height: 100vh; margin: 0px; padding: 0px; display: flex; justify-content: center; align-items: center; background: #F5F5F5">
+  <div style="width: 779px; height: 692px; position: relative; background: white; box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.25); overflow: hidden; border-radius: 20px">
+    
+    <div style="width: 724px; height: 643px; left: 27px; top: 31px; position: absolute; overflow: hidden">
+      
+      <div style="width: 724px; left: 0px; top: 0px; position: absolute; text-align: center; justify-content: center; display: flex; flex-direction: column; color: black; font-size: 64px; font-family: Inter; font-weight: 700; word-wrap: break-word">Admin Portal</div>
+      
+      <div style="width: 353px; left: 185px; top: 92px; position: absolute; text-align: center; justify-content: center; display: flex; flex-direction: column; color: #5B5A5A; font-size: 20.03px; font-family: Inter; font-weight: 400; word-wrap: break-word">Sign in to manage and moderate clubs.</div>
+      
+      <div style="width: 674px; height: 59px; left: 25px; top: 200px; position: absolute">
+        
+        <input type="email" placeholder="Enter your email" style="width: 674px; height: 50px; left: 0px; top: 5px; position: absolute; background: rgba(30, 64, 175, 0); border-radius: 10.02px; border: 1.50px #EEEEEE solid">
+        
+      </div>
+      
+      <div style="width: 674px; height: 59px; left: 25px; top: 298px; position: absolute">
+        
+        <input type="password" placeholder="Enter your password" style="width: 674px; height: 50px; left: 0px; top: 5px; position: absolute; background: rgba(30, 64, 175, 0); border-radius: 10.02px; border: 1.50px #EEEEEE solid">
+        
+        
+      </div>
+      
+      <div style="width: 674px; left: 25px; top: 176px; position: absolute; color: #404040; font-size: 20px; font-family: Inter; font-weight: 400; word-wrap: break-word">
+        Email
+      </div>
+      
+      <div style="width: 674px; left: 25px; top: 274px; position: absolute; color: #404040; font-size: 20px; font-family: Inter; font-weight: 400; word-wrap: break-word">
+        Password
+      </div>
+      
+      <!-- Connect this with Password Recovery Page -->
+      <div style="width: 175px; left: 25px; top: 461px; position: absolute; color: #007D99; font-size: 20px; font-family: Inter; font-weight: 400; word-wrap: break-word">
+        Forgot Password?
+      </div>
+      
+      <div style="width: 674px; height: 59px; left: 25px; top: 372px; position: absolute">
+        
+        <div style="width: 674px; height: 50px; left: 0px; top: 5px; position: absolute; background: #00550A; border-radius: 10.02px"></div>
+
+        <!-- Connect this with Admin Dashboard -->
+        <button style="width: 674px; height: 50px; left: 0px; top: 5px; position: absolute; text-align: center; justify-content: center; display: flex; flex-direction: column; color: white; font-size: 25.04px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+          Sign In As Admin
+        </button>
+      </div>
+      
+      <div style="width: 674px; height: 2px; left: 25px; top: 446px; position: absolute; background: #E6E6E6"></div>
+      <div style="width: 674px; height: 108px; left: 25px; top: 515px; position: absolute; background: #83A0FF; border-radius: 15px"></div>
+      <div style="width: 674px; height: 108px; left: 38px; top: 515px; position: absolute; background: #E9F0FF; overflow: hidden; border-radius: 15px">
+        <div style="width: 258px; height: 72px; left: 208px; top: 18px; position: absolute">
+          
+          <div style="width: 240px; left: 10px; top: 0px; position: absolute; color: #404040; font-size: 20px; font-family: Inter; font-weight: 400; word-wrap: break-word">
+            Are you a Student?
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </body>
   )
 }
-
-export default AdminLogin
