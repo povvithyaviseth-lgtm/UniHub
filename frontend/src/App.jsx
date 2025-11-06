2e10cc4e4c6050283ff0f773c5b75b862a4db08a
Jack Dosti <xdosti@csus.edu>
2025-11-06 09:21:02 -0800

    JACK:Updated: APP.JSX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import AdminLogin from "./pages/AdminLogin.jsx"
+import StudentLogin from "./pages/StudentLogin.jsx"   // 👈 add this import
 
 function App() {
   return (
     <Routes>
-      <Route path="/admin" element={<AdminLogin/>} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path="/student" element={<StudentLogin />} />   {/* 👈 new route */}
       {/* Add more routes here */}
     </Routes>
   )
