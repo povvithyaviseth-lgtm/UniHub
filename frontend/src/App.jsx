678f358459037c62a674c0f9dab22d05db7d0bd4
SankhyaBhatt <sankhyabhatt@csus.edu>
2025-11-06 17:30:52 -0800

    Added index.css to handle hover effects and added the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import AdminLogin from "./pages/AdminLogin.jsx"
 import StudentLogin from "./pages/StudentLogin.jsx"   // 👈 add this import
+import HomePage from "./pages/HomePage.jsx"
 
 function App() {
   return (
     <Routes>
       <Route path="/admin" element={<AdminLogin />} />
       <Route path="/student" element={<StudentLogin />} />   {/* 👈 new route */}
+      <Route path="/home" element={<HomePage />} />
       {/* Add more routes here */}
     </Routes>
   )
