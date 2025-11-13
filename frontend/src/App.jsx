338ce98550fa47cc957e9d3fb451173bc1a1fceb
SankhyaBhatt <sankhyabhatt@csus.edu>
2025-11-12 22:16:18 -0800

    Started set up for admin and club leader console. Created reusable pop-up module that all pop ups can use.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom"
 import AdminLogin from "./pages/AdminLogin.jsx"
 import StudentLogin from "./pages/StudentLogin.jsx"   // 👈 add this import
 import HomePage from "./pages/HomePage.jsx"
+import ClubManagement from "./pages/ClubManagementPage.jsx"
+import AdminPage from "./pages/AdminPage.jsx"
+
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/admin" element={<AdminLogin />} />
       <Route path="/" element={<StudentLogin />} />   {/* 👈 new route */}
       <Route path="/home" element={<HomePage />} />
+      <Route path="/clubManage" element={<ClubManagement />} />
+      <Route path="/adminManage" element={<AdminPage />} />
+      
       {/* Add more routes here */}
     </Routes>
   )
