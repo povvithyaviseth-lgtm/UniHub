d9286d8e2407bcde4e147c22e67f950c87c32417
povvithya viseth <povvithyaviseth@csus.edu>
2025-12-07 23:02:42 -0800

    Revert "deployment fix v3"
    This reverts commit 763c0e6c1a602cd100ec752b1628d22abce1221a.

--- frontend/src/pages/Admin/AdminLogin.jsx
+++ frontend/src/pages/Admin/AdminLogin.jsx
@@ -18,7 +18,7 @@ import {
   studentBox,
   studentText,
   studentLink,
-} from "../../style/AdminLoginPageStyle";
+} from "../../style/AdminLoginPageStyle.jsx";
 
 // ✅ Ensure global font + base styles from index.css are applied
 import "../../index.css";
